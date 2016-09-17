3c054c1ce3ee691ae44905add73c0b4662648031
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-09-17 17:45:12 +0530

    Update

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 var postcss = require('postcss')
+var selectorParser = require('postcss-selector-parser')
 
 module.exports = postcss.plugin('postcss-rscss-linter', function (options, secOptions) {
   return function (root, result) {
     root.walkRules(function (rule) {
-      result = walkRule(rule, result)
+      walkRule(rule, result)
     })
     return result
   }
 })
 
+/*
+ * Internal: Walks a Rule.
+ *
+ * A rule is `.foo, .bar { color: blue }`. The selectors will be found (eg,
+ * `.foo` and `.bar`) and be delegated to `walkSelector()`.
+ */
+
 function walkRule (node, state) {
-  state.warn('hi there', {
+  selectorParser(xform).process(node.selector)
+
+  function xform (selectors) {
+    selectors.nodes.forEach(function (selector) {
+      walkSelector(selector, node, state)
+    })
+  }
+}
+
+/*
+ * Walks a Selector.
+ */
+
+function walkSelector (selector, node, state) {
+  var sel = selector.toString().trim()
+  state.warn('sel: ' + sel, {
     node: node
   })
-
-  return state
 }
--- test.js
+++ test.js
@@ -4,7 +4,10 @@ const linter = require('./index')
 const css = require('fs').readFileSync(__dirname + '/example.css', 'utf-8')
 
 postcss([linter]).process(css).then(result => {
-  console.log('result:', require('util').inspect(result, { depth: null, colors: true }))
+  const warnings = result.warnings()
+  warnings.forEach(warn => {
+    console.log(warn.toString())
+  })
 })
 .catch(err => {
   console.error(err)
