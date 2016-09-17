3ebc464dc605335b642067b4435e5a5d87591dd5
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-09-17 17:58:07 +0530

    Update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var postcss = require('postcss')
 var selectorParser = require('postcss-selector-parser')
 
-module.exports = postcss.plugin('postcss-rscss-linter', function (options, secOptions) {
-  return function (root, result) {
-    root.walkRules(function (rule) {
-      walkRule(rule, result)
-    })
+/*
+ * PostCSS plugin
+ */
+
+module.exports = postcss.plugin('postcss-rscss-linter', (options, secOptions) => {
+  return (root, result) => {
+    root.walkRules(rule => walkRule(rule, result))
     return result
   }
 })
@@ -32,8 +34,53 @@ function walkRule (node, state) {
  */
 
 function walkSelector (selector, node, state) {
+  // For errors.
   var sel = selector.toString().trim()
-  state.warn('sel: ' + sel, {
-    node: node
+
+  // Split it by combinator.
+  var parts = splitBy(selector.nodes, n => n.type === 'combinator')
+
+  parts.forEach((part, idx) => {
+    if (part.type === 'combinator') {
+      if (part.value !== '>') {
+        state.warn(`Missing > operator in '${sel}' (missing-child-descendant)`, { node })
+      }
+    } else {
+      walkSelectorPart(part, idx / 2, node, state)
+    }
   })
+}
+
+/*
+ * Walk a selector part
+ */
+
+function walkSelectorPart (part, idx, node, state) {
+  if (idx === 0) {
+    // Ensure that its a component, a helper, or a tag
+  } else {
+  }
+
+  // state.warn(`${part.toString()}/${idx}: sup`, { node })
+}
+
+/*
+ * Internal: split by a function
+ */
+
+function splitBy (list, fn) {
+  var result = []
+  var section = []
+  list.forEach(function (item, idx) {
+    if (fn(item, idx)) {
+      result.push(section)
+      result.push(item)
+      section = []
+    } else {
+      section.push(item)
+    }
+  })
+
+  result.push(section)
+  return result
 }
