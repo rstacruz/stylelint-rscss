26b163e148bd615c9972fd2ce128ca1206a8f4b1
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-09-17 19:17:32 +0530

    Refactor for stylelint

--- stylelint.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  default: [
-    require('./lib/child-selector')
-  ]
-}
--- test.js
+++ test.js
@@ -1,11 +1,14 @@
-const rules = require('./stylelint')
 const stylelint = require('stylelint')
 
 stylelint.lint({
   files: [__dirname + '/fixtures/example.css'],
 })
   .then(result => {
-    console.log('result:', require('util').inspect(result, { depth: null, colors: true }))
+    result.results.forEach(res => {
+      res.warnings.forEach(warn => {
+        console.log('warn:', require('util').inspect(warn, { depth: null, colors: true }))
+      })
+    })
   })
   .catch(err => {
     throw err
