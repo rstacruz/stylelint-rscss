3b5d42fad634203f8d56ec7b5f79f5b59a142aca
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-09-17 17:20:59 +0530

    Update

--- index.js
+++ index.js
@@ -2,25 +2,17 @@ var postcss = require('postcss')
 
 module.exports = postcss.plugin('postcss-rscss-linter', function (options, secOptions) {
   return function (root, result) {
-    return walk(root, result)
+    root.walkRules(function (rule) {
+      result = walkRule(rule, result)
+    })
+    return result
   }
 })
 
-function walk (node, state) {
-  if (node.type === 'rule') {
-    state = walkRule(node, state)
-  }
-
-  if (node.nodes) {
-    state = node.nodes.reduce(function (state, subnode) {
-      return walk(subnode, state)
-    }, state)
-  }
-
-  return state
-}
-
 function walkRule (node, state) {
-  state.messages.push({ 'rule': node.selector })
+  state.messages.push({
+    position: node.source.start,
+    selector: node.selector
+  })
   return state
 }
--- test.js
+++ test.js
@@ -4,5 +4,5 @@ const linter = require('./index')
 const css = require('fs').readFileSync(__dirname + '/example.css', 'utf-8')
 
 postcss([linter]).process(css).then(result => {
-  console.log(result)
+  console.log('result:', require('util').inspect(result, { depth: null, colors: true }))
 })
