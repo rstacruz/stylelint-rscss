bfb08fc13adaa33486aa12314670698ca69d5342
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-09-18 10:17:49 +0800

    Validate helper names

--- test.js
+++ test.js
@@ -23,17 +23,23 @@ test('child operators', t => {
 test('component name', t => {
   runLint(t, [fixture('component-names.css')], res => {
     const warnings = res.results[0].warnings
-    t.equal(warnings.length, 5)
+    t.equal(warnings.length, 8)
     t.equal(warnings[0].text,
-      "Invalid component name format: '.badcomponent' (rscss/component-name-format)")
+      "Invalid component format: '.badcomponent' (rscss/component-name-format)")
     t.equal(warnings[1].text,
-      "Invalid component name format: '.badcomponent.-xyz' (rscss/component-name-format)")
+      "Invalid component format: '.badcomponent.-xyz' (rscss/component-name-format)")
     t.equal(warnings[2].text,
-      "Invalid component name format: '.badcomponent.-abc' (rscss/component-name-format)")
+      "Invalid component format: '.badcomponent.-abc' (rscss/component-name-format)")
     t.equal(warnings[3].text,
       "Only one component name is allowed: '.too-many.component-names' (rscss/component-name-format)")
     t.equal(warnings[4].text,
-      "Invalid component name format: '._badhelper.-variant' (rscss/component-name-format)")
+      "Invalid helper format: '._badhelper.-variant' (rscss/component-name-format)")
+    t.equal(warnings[5].text,
+      'Invalid component format: \'._badhelper.-variant\' (rscss/component-name-format)')
+    t.equal(warnings[6].text,
+      'Invalid helper format: \'._badhelper.element\' (rscss/component-name-format)')
+    t.equal(warnings[7].text,
+      'Invalid component format: \'._badhelper.element\' (rscss/component-name-format)')
   })
 })
 
