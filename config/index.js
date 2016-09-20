89463d21c774b9c5b88d59b7d916f93cd86b36e6
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-09-20 16:15:54 +0800

    Add more bootstrap exceptions

--- config/index.js
+++ config/index.js
@@ -6,8 +6,12 @@ module.exports = {
     'rscss/no-descendant-combinator': 'always',
     'rscss/class-format': [true, {
       componentWhitelist: [
+        // These are Bootstrap classes that are typically extended as RSCSS components.
+        // These exceptions would be useful in projects that use RSCSS in Bootstrap sites.
         'btn',
-        'container'
+        'container',
+        'checkbox',
+        'radio'
       ]
     }]
   }
